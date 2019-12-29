c3dffc0cab1be77fbda892f5700d1e984ba1f9f3
ClonedOne <severi.giorgio@gmail.com>
2019-12-29 07:49:08 -0500

    added sway config

---
+++ sway/.config/sway/scripts/sway_bar.sh
@@ -0,0 +1,71 @@
+# Change this according to your device
+################
+# Variables
+################
+
+# Keyboard input name
+keyboard_input_name="1:1:AT_Translated_Set_2_keyboard"
+
+# Date and time
+date_and_week=$(date "+%Y/%m/%d (w%-V)")
+current_time=$(date "+%H:%M")
+
+#############
+# Commands
+#############
+
+# Battery or charger
+battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
+battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
+
+# Audio and multimedia
+# audio_volume=$(pamixer --sink `pactl list sinks short | grep SUSPENDED | awk '{print $1}'` --get-volume)
+# audio_is_muted=$(pamixer --sink `pactl list sinks short | grep SUSPENDED | awk '{print $1}'` --get-mute)
+audio_volume=$(pamixer --sink 0 --get-volume)
+audio_is_muted=$(pamixer --sink 0 --get-mute)
+media_artist=$(playerctl metadata artist)
+media_song=$(playerctl metadata title)
+player_status=$(playerctl status)
+
+# Network
+network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
+# interface_easyname grabs the "old" interface name before systemd renamed it
+interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
+ping=$(ping -c 1 www.google.es | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
+
+# Others
+loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
+
+
+if [ $battery_status = "discharging" ];
+then
+    battery_pluggedin='⚠'
+else
+    battery_pluggedin='⚡'
+fi
+
+if ! [ $network ]
+then
+   network_active="⛔"
+else
+   network_active="⇆"
+fi
+
+if [ $player_status = "Playing" ];
+then
+    song_status='▶'
+elif [ $player_status = "Paused" ]
+then
+    song_status='⏸'
+else
+    song_status=''
+fi
+
+if [ $audio_is_muted = "true" ];
+then
+    audio_active='!♪'
+else
+    audio_active='♪'
+fi
+
+echo "$song_status $media_artist - $media_song | $network_active $interface_easyname ($ping ms) | Δ $loadavg_5min | $audio_active $audio_volume% | $battery_pluggedin $battery_charge | $date_and_week $current_time"
